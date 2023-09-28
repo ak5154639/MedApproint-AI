f3ff547355536ad220a31da7c29dcbe342fd1f38
Aniket Kumar Sharma <aniket@Aniket>
2023-09-28 16:38:39 +0530

    login

--- appointApp/urls.py
+++ appointApp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('register-doctor', views.register_doctor, name="register_doctor"),
-    path('register-patient', views.register_patient, name="register_patient")
+    path('register-patient', views.register_patient, name="register_patient"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout")
 ]
